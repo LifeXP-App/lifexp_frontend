dbc6e9665ea17f252c8a9574a473bc95d79ff165
jzjason20 <bargavkrishna26@gmail.com>
2026-02-01 19:23:19 +0530

    fix: update user profile link to point to profile page

--- app/(main)/leaderboard/rookie/page.tsx
+++ app/(main)/leaderboard/rookie/page.tsx
@@ -333,7 +333,7 @@ export default function RookieLeaderboard() {
         ) : (
           <div className="bg-white dark:bg-dark-2 p-6 rounded-xl border border-gray-200 dark:border-gray-800 mb-4">
             <div className="text-center flex flex-col items-center">
-              <Link href={`/user/${currentUser.username}`}>
+              <Link href={`/profile/${currentUser.username}`}>
                 <img
                   src={currentUser.profile_picture}
                   className="h-24 w-24 rounded-full object-cover"
