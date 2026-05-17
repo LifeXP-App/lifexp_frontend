a69ec7014979c43244a8dacbf24c6aacd70af659
jzjason20 <bargavkrishna26@gmail.com>
2026-05-17 22:06:18 +0530

    autoformat indented bruh

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,54 +1,59 @@
 "use client";
 
 import { LEADERBOARD_ENABLED } from "@/src/lib/constants/featureFlags";
-import { NavigationItem } from "./NavigationItem";
 import { useAuth } from "../context/AuthContext";
-
+import { NavigationItem } from "./NavigationItem";
 
 interface NavigationProps {
   accentColor?: string;
 }
 
 export function Navigation({ accentColor }: NavigationProps) {
+  const { me } = useAuth();
+  console.log(me);
 
-const { me } = useAuth();
-console.log(me)
+  const leaderboardNavItem = {
+    label: "Leaderboard",
+    href: "/leaderboard/rookie",
+    active: [
+      "/leaderboard/rookie",
+      "/leaderboard/warrior",
+      "/leaderboard/protagonist",
+      "/leaderboard/diplomat",
+      "/leaderboard/alchemist",
+      "/leaderboard/prodigy",
+      "/leaderboard/goals",
+    ],
+    icon: "trophy",
+  } as const;
 
+  const NAV_ITEMS = [
+    { label: "Feed", href: "/", active: ["/"], icon: "home" },
+    { label: "Search", href: "/search", active: ["/search"], icon: "search" },
+    {
+      label: "Goals",
+      href: "/goals",
+      active: ["/goals", "/a"],
+      icon: "squares",
+    },
 
-const leaderboardNavItem = {
-  label: "Leaderboard",
-  href: "/leaderboard/rookie",
-  active: [
-    "/leaderboard/rookie",
-    "/leaderboard/warrior",
-    "/leaderboard/protagonist",
-    "/leaderboard/diplomat",
-    "/leaderboard/alchemist",
-    "/leaderboard/prodigy",
-    "/leaderboard/goals",
-  ],
-  icon: "trophy",
-} as const;
+    ...(process.env.NEXT_PUBLIC_USER_TYPE === "admin" && LEADERBOARD_ENABLED
+      ? [leaderboardNavItem]
+      : []),
+    {
+      label: "Profile",
+      href: `/u/${me?.username}`,
+      active: ["/u"],
+      icon: "user",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+      active: ["/settings"],
+      icon: "settings",
+    },
+  ];
 
-
-const NAV_ITEMS = [
-  { label: "Feed", href: "/", active: ["/"], icon: "home" },
-  { label: "Search", href: "/search", active: ["/search"], icon: "search" },
-  { label: "Goals", href: "/goals", active: [
-    "/goals",
-    "/a"
-  ], icon: "squares" },
-
-  ...(process.env.NEXT_PUBLIC_USER_TYPE === "admin" && LEADERBOARD_ENABLED
-    ? [leaderboardNavItem]
-    : []),
-  { label: "Profile", href: `/u/${me?.username}`, active: ["/u"], icon: "user" },
-  { label: "Settings", href: "/settings", active: ["/settings"], icon: "settings" },
-];
-
-
-
-  
   return (
     <nav className="px-2 py-2 flex-1">
       <div className="space-y-4">
